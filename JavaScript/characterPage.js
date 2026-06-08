1894ccf46028a98d15f8bab00262cc13503c9b51
bradley <braddersc@aol.com>
2026-06-08 16:29:17 +0100

    Added characterID mapping

--- JavaScript/characterPage.js
+++ JavaScript/characterPage.js
@@ -20,8 +20,9 @@ const characterWisdom = document.querySelector("#character_stats-wisdom");
 const characterCharisma = document.querySelector("#character_stats-charisma");
 
 // NEED TO FIND A WAY TO GRAB CHARACTER ID OF NEW CHARACTER
-
+const characterID = window.location.search;
 async function getCharacter() {
+
     try {
         const res = await fetch(`http://localhost:8080/api/characters/${characterID}`);
         if (!res.ok) { throw new Error(`Response Status: ${response.status}`)}
